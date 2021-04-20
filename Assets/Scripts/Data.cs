4d4d2bc36fe6f7a445dfea31c82f2576c73204b5
ZGV-STUDY110\User <orestbosenko@gmail.com>
2021-04-20 12:32:11 +0300

    Додав клас Data для збереження інформації

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+[Serializable]
+public class Data
+{
+    private float _scoreLevelResult;
+    private float _survivalLevelTime;
+    private int _singleLevelGameCount;
+    private int _cooperativeLevelGameCount;
+    private string _language;
+    Data()
+    {
+        _scoreLevelResult = 0;
+        _survivalLevelTime = 0;
+        _singleLevelGameCount = 0;
+        _cooperativeLevelGameCount = 0;
+        _language = "English";
+    }
+}
